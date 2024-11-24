871ff584dc9379c3a419a92e950ca116c4b0a4c5
Krishna Kant Singh <86555130+kksingh069@users.noreply.github.com>
2024-11-24 23:27:52 +0530

    xfcdgh

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ecommerce-frontend-c7wc.vercel.app/",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
